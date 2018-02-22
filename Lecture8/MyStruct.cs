ffa1fe1cd61ba02cbf5eb527019346f60faa0e5e
Song Wei, Gabriel Wong <e0284002@u.nus.edu>
2018-02-22 22:17:39 +0800

    Added Lecture 8

---
+++ Lecture8/MyStruct.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture8
+{
+    enum Days { Mon, Tue, Wed, Thur, Fri };
+    public struct MyStruct
+    {
+        
+    }
+    class MyStructTest
+    {
+        static void Main()
+        {
+            Days currentDay;
+
+            currentDay = Days.Fri;
+            Console.WriteLine(Days.Fri);
+            currentDay++;
+            Console.WriteLine(currentDay);
+
+        }
+    }
+}
---
+++ Lecture8/Slide30Example.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture8
+{
+    public enum Days2 {Mon, Tue, Wed, Thur, Fri, Sat, Sun};
+    public struct Birthday
+    {
+        private Days2 day;
+        private string month;
+
+        public Days2 BDayOfWeek {
+            get { return day; }
+            set { day = value; }
+        }
+
+        public string BDayMonth
+        {
+            get { return month; }
+            set { month = value; }
+        }
+
+    }
+
+    class Slide30Test
+    {
+        static void Main()
+        {
+            Birthday myBirthday = new Birthday();
+            Console.WriteLine(myBirthday.BDayMonth);
+            Console.WriteLine(myBirthday.BDayOfWeek);
+            myBirthday.BDayMonth = "December";
+            myBirthday.BDayOfWeek = Days2.Mon;
+            Console.WriteLine(myBirthday.BDayMonth);
+            Console.WriteLine(myBirthday.BDayOfWeek);
+        }
+    }
+}
